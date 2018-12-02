60a8976bd96e1a53a559b13686660e49f7d9aee4
Matej Ramuta <matej.ramuta@gmail.com>
2018-12-02 16:41:45 -0500

    localhost comment updated

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ app = webapp2.WSGIApplication([
 ], debug=True)
 
 
-# run the localhost server
-localhost = True  # change to False before deploying to Google Cloud (GAE)
+# run on server
+localhost = True  # True: non-GAE localhost server; False: GAE on either localhost or on Google Cloud
 if localhost:
     def main():
         from paste import httpserver
